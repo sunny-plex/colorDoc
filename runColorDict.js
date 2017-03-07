d1480fe7a441578ede0073049b5ba251312b4ff3
zhaokun.zhang <zhaokun.zhang@sumscope.com>
2017-03-07 16:25:24 +0800

    调整统一配置，增加可读性。

--- runColorDict.js
+++ runColorDict.js
@@ -1,22 +1,22 @@
 /* 设定运行配置并启动颜色处理任务 */
 
-global.colorDocConfig = {
-    /* 文件读写编码 */
-    "encode": "UTF-8",
-    /* 控制台显示级别0,1,2 */
-    "logLevel": 1,
-    /* 要处理的项目根路径 */
-    "srcPath": "./sample",
-    /* 要处理的文件名后缀 */
-    "srcSuffix": ".less",
-    /* 扫描样式文件时忽略的路径和文件名关键字 */
-    "srcIgnore": [
-        "bower_components",
-        "node_modules",
-        ".git"
-    ],
-    /* 输出字典路径 */
-    "colorDictFilePath": "./export/colorDict.less"
-};
+// global.colorDocConfig = {
+//   /* 文件读写编码 */
+//   "encode": "UTF-8",
+//   /* 控制台显示级别0,1,2 */
+//   "logLevel": 1,
+//   /* 要处理的项目根路径 */
+//   "srcPath": "./sample",
+//   /* 要处理的文件名后缀 */
+//   "srcSuffix": ".less",
+//   /* 扫描样式文件时忽略的路径和文件名关键字 */
+//   "srcIgnore": [
+//     "bower_components",
+//     "node_modules",
+//     ".git"
+//   ],
+//   /* 输出字典路径 */
+//   "colorDictFilePath": "./export/colorDict.less"
+// };
 
 var runner = require('./lib/autoVariableColor');
